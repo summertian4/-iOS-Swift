99ea278af41269375dc077fc1ec59bb1d7997629
周凌宇 <coderfish@qq.com>
2016-10-10 11:44:57 +0800

    Update HelloSwift_020.playground

--- Hello Swift/HelloSwift_020.playground/Pages/枚举.xcplaygroundpage/Contents.swift
+++ Hello Swift/HelloSwift_020.playground/Pages/枚举.xcplaygroundpage/Contents.swift
@@ -2,6 +2,20 @@
 
 import Foundation
 
-var str = "Hello, playground"
+enum Month {
+    case January
+    case February
+    case March
+    case April
+    case May
+    case June
+    case July
+    case August
+    case September
+    case November
+    case December
+}
+
+let currentMonth = Month.July
 
 //: [Next](@next)
