4d1b98cef66e46f1d7e8575387744cc9cfb8782a
周凌宇 <coderfish@qq.com>
2016-10-28 10:52:40 +0800

    Update HelloSwift_020.playground

--- Hello Swift/HelloSwift_020.playground/Pages/枚举.xcplaygroundpage/Contents.swift
+++ Hello Swift/HelloSwift_020.playground/Pages/枚举.xcplaygroundpage/Contents.swift
@@ -2,7 +2,8 @@
 
 import Foundation
 
-enum Month {
+// Raw Value
+enum Month: Int {
     case January
     case February
     case March
@@ -16,6 +17,34 @@ enum Month {
     case December
 }
 
+enum Direction: Int {
+    case Left = 1, Right, Up, Down
+}
+
 let currentMonth = Month.July
+let currentMonth2: Month = .July
+
+Direction.Down
+
+func mothsBeforeNewYear(month: Month) -> Int {
+    return 12 - month.rawValue
+}
+
+let input = 4
+// 返回可选型
+if let month = Month(rawValue: input) {
+    mothsBeforeNewYear(month: month)
+}
+
+// swift 中 enum 不仅仅是 Int 型
+enum ProgrammingLanguage: String {
+    case Swift
+    case ObjectiveC = "Objective-C"
+    case C = "C"
+    case JAVA
+}
+
+let language: ProgrammingLanguage = .Swift
+print("\(language)")
 
 //: [Next](@next)
