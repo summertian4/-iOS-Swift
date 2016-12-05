a16c9b2f04d4714a5692dcb96b070142e659b244
周凌宇 <coderfish@qq.com>
2016-12-05 18:27:57 +0800

    Add HelloSwift_040.playground

---
+++ Hello Swift/HelloSwift_040.playground/Contents.swift
@@ -0,0 +1,53 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class Something {
+    init(){
+        
+    }
+}
+
+/// hahah
+///
+/// hahah
+/// 
+/// - LIST 1
+/// - LIST 2
+/// - LIST 4
+///
+/// - Returns: a string
+func showMultilineComments() -> String {
+    let text = "You can use the /** ... */ for multiline comments"
+    return text
+}
+
+/**
+ # Title
+ 
+ # subtitle
+ 
+ 1. LIST 1
+ 2. LIST 1
+ 3. LIST 1
+ 
+ - Parameter item1: 第一个参数
+ - Parameter item2: 第二个参数
+ - Returns: 返回值
+ - Throws: 一个异常
+ - Precondition: 前置条件
+ - Postcondition: 循环不变量
+ - Requires: 所需内容
+ - Invariant: 循环不变量
+ - Complexity: 复杂度
+ - Important: 重要信息
+ - Warning: 警告信息
+ - Attention: 警告信息
+ - Note: 记录
+ - Remark: 评论
+ 
+ */
+func showMultilineComments2(item1: String, item2: Int) throws -> String {
+    let text = "You can use the /** ... */ for multiline comments"
+    return text
+}
