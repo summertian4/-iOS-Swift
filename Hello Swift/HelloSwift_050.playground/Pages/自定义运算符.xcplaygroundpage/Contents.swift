0548a1317ba39d87c01ff5c64ad8a7e25ff55f2a
周凌宇 <coderfish@qq.com>
2016-12-15 14:36:41 +0800

    Update HelloSwift_050.playground

--- Hello Swift/HelloSwift_050.playground/Pages/自定义运算符.xcplaygroundpage/Contents.swift
+++ Hello Swift/HelloSwift_050.playground/Pages/自定义运算符.xcplaygroundpage/Contents.swift
@@ -2,6 +2,59 @@
 
 import Foundation
 
-var str = "Hello, playground"
+struct Vector3 {
+    // 加法运算符重载
+    static func +(left: Vector3, right: Vector3) -> Vector3 {
+        return Vector3()
+    }
+    
+    static func ==(left: Vector3, right: Vector3) -> Bool {
+        return false
+    }
+}
+
+var v: Vector3 = Vector3()
+
+// MARK: - 单目运算符
+// 后置运算 先声明
+postfix operator +++
+postfix func +++( vector: inout Vector3) -> Vector3 {
+    return Vector3()
+}
+
+v+++
+
+// 前置运算
+prefix operator +++
+prefix func +++( vector: inout Vector3) -> Vector3 {
+    return Vector3()
+}
+
++++v
+
+// MARK: - 双目运算符
+precedencegroup ComparisonPrecedence {
+    associativity: left
+    higherThan: LogicalConjunctionPrecedence
+}
+
+infix operator ^: ComparisonPrecedence
+func ^(left: Vector3, right: Vector3) -> Double {
+    return 0.0
+}
+
+v^v
+
+
+
+// ===============================================================
+
+func 😀(aString: String, bString: String) -> String {
+    return aString + "😀" + bString
+}
+
+😀(aString: "😁", bString: "😃")
+
+
 
 //: [Next](@next)
--- Hello Swift/HelloSwift_050.playground/Pages/运算符重载.xcplaygroundpage/Contents.swift
+++ Hello Swift/HelloSwift_050.playground/Pages/运算符重载.xcplaygroundpage/Contents.swift
@@ -2,6 +2,15 @@
 
 import Foundation
 
-var str = "Hello, playground"
+struct Vector3 {
+    // 加法运算符重载
+    static func +(left: Vector3, right: Vector3) -> Vector3 {
+        return Vector3()
+    }
+    
+    static func ==(left: Vector3, right: Vector3) -> Bool {
+        return false
+    }
+}
 
 //: [Next](@next)
